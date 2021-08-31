7de735ee920ef05e494ad26e30763073a0dae7fa
DaviDias26 <davi.dias2021@outlook.com>
2021-08-30 22:03:00 -0300

    30-08

---
+++ web/src/pages/petshop/index.js
@@ -0,0 +1,31 @@
+import Header from "../../components/header";
+import "./style.css"
+
+const Petshop = () => {
+    return(
+        <div className="h-100">
+            <Header/>
+            <div className="container">
+                <div className="row">
+                    <div className="col-2">
+                        <img src="https://www.petlove.com.br/static/uploads/banner_image/image/4304/logo-petlove-push.png" className="img-fluid petshop-image"/>
+                        <b>PetLove</b>
+                        <div className="petshop-infos">
+                            <span class="mdi mdi-star"></span>
+                            <text>
+                                <b>2,8</b>
+                            </text>
+                            <span class="mdi mdi-cash-usd-outline"></span>
+                            <text>$$$</text>
+                            <span class="mdi mdi-crosshairs-gps"></span>
+                            <text>1,9km</text>
+                        </div>
+                        <label className="badge bg-primary">Frete Grátis</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Petshop;
